699d916783a5054a48c3a1a60bd480ba8a001c7b
Fabio Amorelli <amorelli.ff@gmail.com>
2022-08-09 13:21:54 -0300

    fix editUrl

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -37,14 +37,14 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/fabioavf/byron-wiki',
+            'https://github.com/fabioavf/byron-wiki/blob/main',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/fabioavf/byron-wiki',
+            'https://github.com/fabioavf/byron-wiki/blob/main',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
