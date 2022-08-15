bacbecf523c9940b92ef7511696def105c84873b
Fabio Amorelli <amorelli.ff@gmail.com>
2022-08-15 10:57:34 -0300

    fix links

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -15,7 +15,7 @@ function HomepageHeader() {
         <h1 className='hero__title'>{siteConfig.title}</h1>
         <p className='hero__subtitle'>{siteConfig.tagline}</p>
         <div className={styles.buttons}>
-          <Link className='button button--secondary button--lg' to='/docs/guia'>
+          <Link className='button button--secondary button--lg' to='/docs/wiki'>
             Docusaurus Tutorial - 5min ⏱️
           </Link>
         </div>
